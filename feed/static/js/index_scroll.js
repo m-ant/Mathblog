051f262644328b117d01ab25624124efa9f99a17
Михайло Антощук < >
2016-07-18 19:24:45 -0400

    v1.01

--- feed/static/js/index_scroll.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.call-to-action').click(function(){
-        $(this).addClass('active');
-       $('html, body').animate({scrollTop:$('#content').position().top - 60}, 3000);
-    });
-});
